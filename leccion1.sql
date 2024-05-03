1853305fa274f4b11cc655f310ee2b7e954f7eb6
maria <mpramos78915400@gmail.com>
2024-05-03 14:47:41 -0400

    creacion de tablas y select

---
+++ leccion1.sql
@@ -0,0 +1,8 @@
+select titulo as title from movie
+select director from movie
+
+select titulo || '😀' || director as titleAnddirector from movie
+
+select titulo || ' ' || anio as titleyear from movie
+
+select * from movie
